5cd2c155849d5e25eaf8c0e5f3615aad505a587c
Abhiyank51 <abhiyankyadav51@gmail.com>
2024-06-04 21:57:27 +0530

    first commit

---
+++ pdf/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import PDFFile, GradingCriteria
+
+class PDFFileForm(forms.ModelForm):
+    class Meta:
+        model = PDFFile
+        fields = ('file',)
+
+class GradingCriteriaForm(forms.ModelForm):
+    class Meta:
+        model = GradingCriteria
+        fields = ['keywords', 'grading_type']
+
+class KeywordForm(forms.ModelForm):
+    class Meta:
+        model = GradingCriteria
+        fields = ('keywords', 'grading_type', 'min_words', 'max_words')
+
